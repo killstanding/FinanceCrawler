d4b888fea481bd42334d7fa88a279473afd7226c
killsanding <w5743216@163.com>
2015-10-12 14:37:56 +0800

    添加编码集常量类

---
+++ src/main/java/com/jerry/financecrawler/commons/CommonsCharset.java
@@ -0,0 +1,11 @@
+package com.jerry.financecrawler.commons;
+
+/**
+ * Created by Jerry on 2015/10/10.
+ */
+public class CommonsCharset {
+    public static final String GB2312 = "gb2312";
+
+    public static final String UTF_8 = "utf-8";
+
+}
