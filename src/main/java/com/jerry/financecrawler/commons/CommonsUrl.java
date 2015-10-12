6ccf48470cd919c6abc4e3ab6277a0a10182d9f2
killsanding <w5743216@163.com>
2015-10-12 14:31:58 +0800

    添加howbuy 历史净值url和私募主页的url

--- src/main/java/com/jerry/financecrawler/commons/CommonsUrl.java
+++ src/main/java/com/jerry/financecrawler/commons/CommonsUrl.java
@@ -6,7 +6,8 @@ package com.jerry.financecrawler.commons;
 public class CommonsUrl {
 
     public static final String EAST_FINANCE_URL = "http://simu.eastmoney.com/Handler/SimuJJDataAPI.ashx?callback=fundData&sf=PDATE&st=desc&ft=&pi=%s&pn=%s&m=1";
-
+    public static final String HOW_BUY_FINANCE_URL = "http://simu.howbuy.com/mlboard.htm?page=%s&perPage=%s";
+    public static final String HOW_BUY_FINANCE_HISTORICAL_NET_URL = "http://simu.howbuy.com/touzi/%s/lsjz";
 
     public static String getUrl(String baseUrl,int index, int pageNum, int totalPage) {
         String url = "";
@@ -16,5 +17,9 @@ public class CommonsUrl {
             url = String.format(baseUrl,index,totalPage);
         }
         return url;
+    }
+
+    public static String getHowBuyHistoricalNet(String baseUrl,String code){
+        return String.format(baseUrl, code);
     }
 }
