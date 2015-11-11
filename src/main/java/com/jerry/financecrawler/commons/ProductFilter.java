390cb2b2402905827b054109792452f495fadf91
killstanding <w5743216@163.com>
2015-11-11 14:04:30 +0800

    过滤产品名称和产品类型 公共类

---
+++ src/main/java/com/jerry/financecrawler/commons/ProductFilter.java
@@ -0,0 +1,52 @@
+package com.jerry.financecrawler.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jerry on 15/11/9.
+ */
+public class ProductFilter {
+
+    public static final List<String> typeList = new ArrayList<String>(); //类型集合
+    public static final List<String> nameList = new ArrayList<String>(); //类型集合
+    static{
+        //typeList
+        typeList.add("套利型");
+        typeList.add("市场中性");
+        typeList.add("量化对冲");
+        //nameList
+        nameList.add("量化");
+        nameList.add("对冲");
+        nameList.add("阿尔法");
+        nameList.add("CTA");
+        nameList.add("高频");
+        nameList.add("套利");
+        nameList.add("alpha");
+        nameList.add("程序化");
+    }
+
+    //过滤type
+    public static Boolean filterType(String type){
+        return typeList.contains(type);
+    }
+
+    public static boolean filterName(String name){
+        boolean result = false;
+        for (int i = 0; i < nameList.size(); i++) {
+            String midName = nameList.get(i);
+            if(name.indexOf(midName) != -1){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean filter(String type, String name){
+        boolean result = false;
+        if(filterType(type)) result = true;
+        if(filterName(name)) result = true;
+        return result;
+    }
+}
