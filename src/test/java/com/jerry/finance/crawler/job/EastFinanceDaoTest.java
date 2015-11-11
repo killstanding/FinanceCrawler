3dd9afbcf8eca4b6cfe0e36f20fb65bbb77187c4
killstanding <w5743216@163.com>
2015-11-11 14:16:36 +0800

    修改东方财富dao测试类

--- src/test/java/com/jerry/finance/crawler/job/EastFinanceDaoTest.java
+++ src/test/java/com/jerry/finance/crawler/job/EastFinanceDaoTest.java
@@ -1,12 +1,15 @@
 package com.jerry.finance.crawler.job;
 
 import com.jerry.finance.crawler.TestBase;
+import org.junit.Test;
 
 /**
  * Created by Jerry on 2015/9/20.
  */
-public class EastFinanceDaoTest extends TestBase{
-
-
+public class EastFinanceDaoTest extends TestBase {
+    @Test
+    public void test() {
+        System.out.println("11111111");
+    }
 
 }
