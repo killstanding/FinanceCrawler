7d5ed16b0d43e21274ca6e2a64ad7f6e2b356ee1
killstanding <w5743216@163.com>
2015-11-23 16:29:11 +0800

    添加howbuy产品详情url

--- src/main/java/com/jerry/financecrawler/commons/CommonsUrl.java
+++ src/main/java/com/jerry/financecrawler/commons/CommonsUrl.java
@@ -8,18 +8,20 @@ public class CommonsUrl {
     public static final String EAST_FINANCE_URL = "http://simu.eastmoney.com/Handler/SimuJJDataAPI.ashx?callback=fundData&sf=PDATE&st=desc&ft=&pi=%s&pn=%s&m=1";
     public static final String HOW_BUY_FINANCE_URL = "http://simu.howbuy.com/mlboard.htm?page=%s&perPage=%s";
     public static final String HOW_BUY_FINANCE_HISTORICAL_NET_URL = "http://simu.howbuy.com/touzi/%s/lsjz";
+    public static final String HOW_BUY_FINANCE_PRODUCT_DETAIL_URL = "http://simu.howbuy.com/touzi/%s";
 
-    public static String getUrl(String baseUrl,int index, int pageNum, int totalPage) {
+
+    public static String getUrl(String baseUrl, int index, int pageNum, int totalPage) {
         String url = "";
         if (pageNum <= totalPage || totalPage == 0) {
-             url = String.format(baseUrl, index, pageNum);
-        }else{
-            url = String.format(baseUrl,index,totalPage);
+            url = String.format(baseUrl, index, pageNum);
+        } else {
+            url = String.format(baseUrl, index, totalPage);
         }
         return url;
     }
 
-    public static String getHowBuyHistoricalNet(String baseUrl,String code){
+    public static String getUrlByCode(String baseUrl, String code) {
         return String.format(baseUrl, code);
     }
 }
