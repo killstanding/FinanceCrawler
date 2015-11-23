1770e476ea258c39125612b493da51aa382f5122
killstanding <w5743216@163.com>
2015-11-23 16:21:35 +0800

    添加产品详情采集的任务配置和时间配置

--- src/test/java/com/jerry/finance/crawler/db/dao/FundProductDaoTest.java
+++ src/test/java/com/jerry/finance/crawler/db/dao/FundProductDaoTest.java
@@ -1,7 +1,7 @@
 package com.jerry.finance.crawler.db.dao;
 
 import com.jerry.finance.crawler.TestBase;
-import com.jerry.financecrawler.db.dao.IFundProductDao;
+import com.jerry.financecrawler.db.dao.IFundProduct;
 import com.jerry.financecrawler.db.po.FundProductPo;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -18,7 +18,7 @@ public class FundProductDaoTest extends TestBase {
      */
     private static final Logger log = LoggerFactory.getLogger(FundProductDaoTest.class);
     @Resource
-    private IFundProductDao fundProductDao;
+    private IFundProduct fundProductDao;
 
     @Test
     public void testFind() {
