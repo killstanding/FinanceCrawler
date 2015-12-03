d15a198530531559de366405cb46a159915672f1
killstanding <w5743216@163.com>
2015-12-03 14:40:25 +0800

    添加所有的产品数据保存公共方法

---
+++ src/main/java/com/jerry/financecrawler/save/SaveData.java
@@ -0,0 +1,235 @@
+package com.jerry.financecrawler.save;
+
+import com.jerry.financecrawler.commons.ProductFilter;
+import com.jerry.financecrawler.convert.SetValue;
+import com.jerry.financecrawler.convert.VoToPo;
+import com.jerry.financecrawler.db.dao.*;
+import com.jerry.financecrawler.db.po.*;
+import com.jerry.financecrawler.vo.*;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jerry on 15/11/24.
+ */
+@Component
+public class SaveData {
+    @Resource
+    private IIncome incomeDao;
+    @Resource
+    private IFundProduct fundProductDao;
+    @Resource
+    private IHistoricalNet historicalNetDao;
+    @Resource
+    private IAnnualFluctuationRate annualFluctuationRateDao;
+    @Resource
+    private IAnnualizedReturnRate annualizedReturnRateDao;
+    @Resource
+    private IMaximumReturn maximumReturnDao;
+    @Resource
+    private ISharpRatio sharpRatioDao;
+    @Resource
+    private ICalmarRatio calmarRatioDao;
+    @Resource
+    private ISterlingRatio sterlingRatioDao;
+    @Resource
+    private IncomeRankingDao incomeRankingDao;
+
+    public List<FundProductPo> saveFundProductData(List<FundProductVo> fundProductVoList) throws Exception{
+        List<FundProductPo> result = new ArrayList<FundProductPo>();
+        if (fundProductVoList != null) {
+            List<FundProductVo> noExistVos = new ArrayList<FundProductVo>();
+            Integer maxId = fundProductDao.getMaxId();
+            if (maxId == null) maxId = 0;
+            for (int i = 0; i < fundProductVoList.size(); i++) {
+                FundProductVo midVo = fundProductVoList.get(i);
+                String product_name = midVo.getProduct_name();
+                String product_code = midVo.getProduct_code();
+                String product_type = midVo.getProduct_TYPE();
+                if (ProductFilter.filter(product_type, product_name)) {
+                    FundProductPo basePo = fundProductDao.findByCodeOrName(product_code, product_name);
+                    if (basePo != null) {
+                        SetValue.setFundProductPoValue(basePo, midVo);
+                        fundProductDao.modify(basePo);
+                        result.add(basePo);
+                        //System.out.println("basePo = [" + basePo + "]");
+                        // 收益
+                        saveIncomeData(midVo.getIncomeVo(), basePo.getId());
+                    } else {
+                        noExistVos.add(midVo);
+                    }//if  else
+
+                }//if
+            }// for
+            // 保存不存在数据
+            for (int i = 0; i < noExistVos.size(); i++) {
+                //产品
+                FundProductVo midVo = fundProductVoList.get(i);
+                FundProductPo midPo = changeToPo(midVo);
+                midPo.setId(maxId + i + 1);
+                fundProductDao.save(midPo);
+                result.add(midPo);
+                // 收益
+                IncomePo incomePo =midPo.getIncomePo();
+                incomePo.setProduct_id(midPo.getId());
+                incomeDao.save(incomePo);
+            }//for
+
+        }// if
+        return result;
+    }
+
+    private FundProductPo changeToPo(FundProductVo vo) {
+        //产品
+        FundProductPo po = VoToPo.fundProductVoToPo(vo);
+
+        //收益
+        IncomeVo incomeVo = vo.getIncomeVo();
+        IncomePo incomePo = VoToPo.incomeVoToPo(incomeVo);
+        po.setIncomePo(incomePo);
+        return po;
+    }
+
+    public void saveHistoricalNetData(List<HistoricalNetVo> historicalNetList) {
+        if (historicalNetList != null) {
+            Integer maxId = historicalNetDao.getMaxId();
+            if(maxId == null) maxId = 0;
+            for (int i = 0; i < historicalNetList.size(); i++) {
+                HistoricalNetVo midVo = historicalNetList.get(i);
+                HistoricalNetPo midPo = VoToPo.historicalNetVoToPo(midVo);
+                String product_code = midPo.getProduct_code();
+                String net_worth_date = midPo.getNet_worth_date();
+                if (historicalNetDao.findByProductIDAndDate(product_code, net_worth_date) == null) {
+                    if (midPo.getNet_worth_date() != null && !midPo.getNet_worth_date().equals("")) {
+                        midPo.setId(maxId + i + 1);
+                        historicalNetDao.save(midPo);
+                    }//if
+                }//if
+            }//for
+        }//if
+    }
+
+    public void saveFundProductData(FundProductVo vo, int id) throws Exception {
+        String product_name = vo.getProduct_name();
+        String product_code = vo.getProduct_code();
+        FundProductPo po = fundProductDao.findByCodeOrName(product_code, product_name);
+        if (po != null) {
+            SetValue.setFundProductPoValue(po, vo);
+            fundProductDao.modify(po);
+        } else {
+            po = VoToPo.fundProductVoToPo(vo);
+            po.setId(id);
+            fundProductDao.save(po);
+        }//if  else
+
+    }
+
+    //收益
+    public void saveIncomeData(IncomeVo vo, int product_id) throws Exception {
+        IncomePo po = incomeDao.find(product_id);
+        if (po != null) {
+            SetValue.setIncomePoValue(po, vo);
+            incomeDao.modify(po);
+        } else {
+            po = VoToPo.incomeVoToPo(vo);
+            po.setProduct_id(product_id);
+            incomeDao.save(po);
+        }
+    }
+
+    //年化收益率
+    public void saveAnnualizedReturnRateData(AnnualizedReturnRateVo vo, int product_id) throws Exception {
+        AnnualizedReturnRatePo po = annualizedReturnRateDao.find(product_id);
+        if (po != null) {
+            SetValue.setAnnualizedReturnRatePoValue(po, vo);
+            annualizedReturnRateDao.modify(po);
+        } else {
+            po = VoToPo.annualizedReturnRateVoToPo(vo);
+            po.setProduct_id(product_id);
+            annualizedReturnRateDao.save(po);
+        }
+    }
+
+    //年化波动率
+    public void saveAnnualFluctuationRateData(AnnualFluctuationRateVo vo, int product_id) throws Exception {
+        AnnualFluctuationRatePo po = annualFluctuationRateDao.find(product_id);
+        if (po != null) {
+            SetValue.setAnnualFluctuationRatePoValue(po, vo);
+            annualFluctuationRateDao.modify(po);
+        } else {
+            po= VoToPo.annualFluctuationRateVoToPo(vo);
+            po.setProduct_id(product_id);
+            annualFluctuationRateDao.save(po);
+        }
+    }
+
+    //最大回撤
+    public void saveMaximumReturnData(MaximumReturnVo vo, int product_id) throws Exception {
+        MaximumReturnPo po = maximumReturnDao.find(product_id);
+        if (po != null) {
+            SetValue.setMaximumReturnPoValue(po, vo);
+            maximumReturnDao.modify(po);
+        } else {
+            po= VoToPo.maximumReturnVoToPo(vo);
+            po.setProduct_id(product_id);
+            maximumReturnDao.save(po);
+        }
+    }
+
+    //夏普比率
+    public void saveSharpRatioData(SharpRatioVo vo, int product_id) throws Exception {
+        SharpRatioPo po = sharpRatioDao.find(product_id);
+        if (po != null) {
+            SetValue.setSharpRatioPoValue(po, vo);
+            sharpRatioDao.modify(po);
+        } else {
+            po= VoToPo.sharpRatioVoToPo(vo);
+            po.setProduct_id(product_id);
+            sharpRatioDao.save(po);
+        }
+    }
+
+    //CALMAR比率
+    public void saveCalmarRatioData(CalmarRatioVo vo, int product_id) throws Exception {
+        CalmarRatioPo po = calmarRatioDao.find(product_id);
+        if (po != null) {
+            SetValue.setCalmarRatioPoValue(po, vo);
+            calmarRatioDao.modify(po);
+        } else {
+            po= VoToPo.calmarRatioVoToPo(vo);
+            po.setProduct_id(product_id);
+            calmarRatioDao.save(po);
+        }
+    }
+
+    //Sterling比率
+    public void saveSterlingRatioData(SterlingRatioVo vo, int product_id) throws Exception {
+        SterlingRatioPo po = sterlingRatioDao.find(product_id);
+        if (po != null) {
+            SetValue.setSterlingRatioPoValue(po, vo);
+            sterlingRatioDao.modify(po);
+        } else {
+            po= VoToPo.sterlingRatioVoToPo(vo);
+            po.setProduct_id(product_id);
+            sterlingRatioDao.save(po);
+        }
+    }
+
+    //Sterling比率
+    public void saveIncomeRankingData(IncomeRankingVo vo, int product_id) throws Exception {
+        IncomeRankingPo po = incomeRankingDao.find(product_id);
+        if (po != null) {
+            SetValue.setIncomeRankingPoValue(po, vo);
+            incomeRankingDao.modify(po);
+        } else {
+            po= VoToPo.incomeRankingVoToPo(vo);
+            po.setProduct_id(product_id);
+            incomeRankingDao.save(po);
+        }
+    }
+
+
+}
