37a8e33dd5f885ef977711e53f589d7d11b4699d
killstanding <w5743216@163.com>
2015-12-03 15:14:21 +0800

    添加EAST_MONEY的采集url，添加licai的采集url，添加getEastMoneyFinanceHistoricalNetUrlByCode方法

--- src/main/java/com/jerry/financecrawler/commons/CommonsUrl.java
+++ src/main/java/com/jerry/financecrawler/commons/CommonsUrl.java
@@ -5,11 +5,18 @@ package com.jerry.financecrawler.commons;
  */
 public class CommonsUrl {
 
-    public static final String EAST_FINANCE_URL = "http://simu.eastmoney.com/Handler/SimuJJDataAPI.ashx?callback=fundData&sf=PDATE&st=desc&ft=&pi=%s&pn=%s&m=1";
+    //how buy
     public static final String HOW_BUY_FINANCE_URL = "http://simu.howbuy.com/mlboard.htm?page=%s&perPage=%s";
     public static final String HOW_BUY_FINANCE_HISTORICAL_NET_URL = "http://simu.howbuy.com/touzi/%s/lsjz";
     public static final String HOW_BUY_FINANCE_PRODUCT_DETAIL_URL = "http://simu.howbuy.com/touzi/%s";
+    //east money
+    public static final String EAST_MONEY_URL = "http://simu.eastmoney.com/Handler/SimuJJDataAPI.ashx?callback=fundData&sf=HY&st=desc&ft=&pi=%s&pn=%s&m=0&yxzt=1&clsc=-1&glgm=-1&glgs=&v=0.1882981201633811";
+    public static final String EAST_MONEY_HISTORICAL_NET_URL = "http://simu.eastmoney.com/PinzhongF10DataApi.aspx?type=lsjz&fc=%s&pageindex=%s&pagesize=%s";
+    public static final String EAST_MONEY_DETAIL_URL = "http://simu.eastmoney.com/pro%s.html";
+    //licai
+    public static final String LI_CAI_URL = "http://www.licai.com/simu/phtype4.html";
 
+    public static final String LI_CAI_BASE_URL = "http://www.licai.com/";
 
     public static String getUrl(String baseUrl, int index, int pageNum, int totalPage) {
         String url = "";
@@ -23,5 +30,15 @@ public class CommonsUrl {
 
     public static String getUrlByCode(String baseUrl, String code) {
         return String.format(baseUrl, code);
+    }
+
+    public static String getEastMoneyFinanceHistoricalNetUrlByCode(String baseUrl,String fincode, int index, int pageNum, int totalPage){
+        String url = "";
+        if (pageNum <= totalPage || totalPage == 0) {
+            url = String.format(baseUrl, fincode, index, pageNum);
+        } else {
+            url = String.format(baseUrl,fincode, index, totalPage);
+        }
+        return url;
     }
 }
