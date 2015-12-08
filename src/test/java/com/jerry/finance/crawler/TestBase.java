67575b19ff9e79b843ea823b11d1c79aa90b56ed
killstanding <w5743216@163.com>
2015-12-08 15:27:12 +0800

    修改读取默认配置文件字符串

--- src/test/java/com/jerry/finance/crawler/TestBase.java
+++ src/test/java/com/jerry/finance/crawler/TestBase.java
@@ -7,6 +7,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by Jerry on 2015/9/20.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:applicationContext*.xml")
+@ContextConfiguration(locations = "classpath*:applicationContext*.xml")
 public class TestBase {
 }
